ef2b6cc6ac7780f761f8e4f51086cd3083f9be38
ARIJ ARMAN MANDAL <67367001+Arij-arman@users.noreply.github.com>
2020-12-25 13:57:24 +0530

    Update python-calculator.py

--- python-calculator.py
+++ python-calculator.py
@@ -7,7 +7,7 @@
 ########################################
 #A Simple python code using only IF-ELIF-ELSE conditios .
 i = 1
-while 1 :
+while 1  :
 	print('###########_W_E_L_C_O_M_E###########')
 	print('  ________________________________')
 	print('  ___________CALCULATOR___________')
@@ -16,26 +16,27 @@ while 1 :
 	print('[2] Substract or Minus')
 	print('[3] Multiply or into(x)')
 	print('[4] Division or x/y')
-	user = int(input('(@)_Choose a number[1-4]: '))
+	user = float(input('(@)_Choose a number[1-4]: '))
 	if user == 1 :
-		num1 = input('[*] Enter the 1st number: ')
-		num2 = input('[*] Enter the 2nd number: ')
+		num1 = float(input('[*] Enter the 1st number: '))
+		num2 = float(input('[*] Enter the 2nd number: '))
 		print('>>>>Answer is= ',num1+num2)
 	elif user == 2 :
-		num1 = input('[*] Enter the 1st number: ')
-		num2 = input('[*] Enter the 2nd number: ')	
+		num1 = float(input('[*] Enter the 1st number: '))
+		num2 = float(input('[*] Enter the 2nd number: '))	
 		print('>>>>Answer is= ',num1-num2)
 	elif user == 3 :
-		num1 = input('[*] Enter the 1st number: ')
-		num2 = input('[*] Enter the 2nd number: ')	
+		num1 = float(input('[*] Enter the 1st number: '))
+		num2 = float(input('[*] Enter the 2nd number: '))	
 		print('>>>>Answer is= ',num1*num2)
 	elif user == 4 :
-		num1 = input('[*] Enter the 1st number: ')
-		num2 = input('[*] Enter the 2nd number: ')	
+		num1 = float(input('[*] Enter the 1st number: '))
+		num2 = float(input('[*] Enter the 2nd number: '))	
 		print('>>>>Answer is= ',num1/num2)
 	elif user == 99 :
 		print('>>>> Exited claculator. Have a nice day...')
 		exit()
 	else  :
 		print('>>>> Error! Invalid input. Try 1 or 2 or 3 or 4')
+	
 i += 1
